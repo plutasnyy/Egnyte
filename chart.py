92ab33fd775433316ede5bc0628fe0bdf4df9ede
plutasnyy <kamil.plucinski97@gmail.com>
2017-06-15 00:28:59 +0200

    first wykres done...

--- chart.py
+++ chart.py
@@ -1,23 +1,31 @@
 from chart_abstract import *
 import matplotlib.pyplot as plt
-
+import matplotlib.ticker as mticker
+import numpy as np
 class First(Chart):
 
     def __init__(self):
-        self.dict={}
+        self.data_dict={}
 
     def update(self,new_line):
         action=new_line['eventBody']['action']
-        self.dict[action]=self.dict.get(action,0)+1
+        self.data_dict[action]=self.data_dict.get(action,0)+1
 
     def print_chart(self):
-        self.dict=sorted(self.dict.items(), key=lambda x: x[1])
-        X=[ X for (X,Y) in self.dict]
-        Y=[ int(Y) for (X,Y) in self.dict]
+        data=sorted(self.data_dict.items(), key=lambda x: x[1])
+        X=[i for i,k in data]
+        Y=[k for i,k in data]
         ind=range(1,len(X)+1)
-        print(len(X),len(Y))
-        X=range(len(X))
-        plt.bar(ind,Y)
-        plt.xticks(ind,X)
+
+        plt.title("Wykres rzeczy")
+        plt.bar(ind,Y,width=0.85, align='center')
+        plt.xticks(ind, X, rotation='vertical')
+        plt.margins(0.015)
+        plt.subplots_adjust(bottom=0.42)
         plt.yscale('log')
-        plt.savefig('lol.jpg')
+        plt.ylim(0,max(Y)*3)
+
+        for x,y in zip(ind,Y):
+            plt.text(x,y*1.5,y,ha='center',size='xx-small')
+
+        plt.savefig('lalala',dpi=300)
